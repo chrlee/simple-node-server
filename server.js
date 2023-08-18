28d88a03a901776f2670e0a45389ed59b93abc0b
Christian Lee <cllz@amazon.com>
2023-08-18 07:44:44 -0400

    Initial project setup

--- server.js
+++ server.js
@@ -1,14 +1,13 @@
-const http = require('http')
+const express = require('express')
+const app = express()
 
 const hostname = '127.0.0.1'
 const port = 3000
 
-const server = http.createServer((req, res) => {
-    res.statusCode = 200
-    res.setHeader('Content-Type', 'text/plain')
-    res.end('Hello World\n')
-})
+app.get('/', ((req, res) => {
+  res.sendFile('index.html', {root: __dirname})
+}))
 
-server.listen(port, hostname, () => {
+app.listen(port, hostname, () => {
     console.log(`Server running at http://${hostname}:${port}/`)
 })
